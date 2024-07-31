ac372de9919068ab75f417b9959edd62965e3ed0
02chan <snoo_p@naver.com>
2024-07-31 16:17:41 +0900

    7/31
    일지.md 참고

--- main.py
+++ main.py
@@ -6,21 +6,45 @@ from tokens import lostark_token, discord_token
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=intents)
 
 headers = {
-    'accept' : 'application/json',
-    'authorization' : lostark_token
+    'accept': 'application/json',
+    'authorization': lostark_token
 }
 
+# 사용자가 입력한 캐릭터 이름을 기반으로 URL 생성
+def create_url(character_name):
+    base_url = "https://developer-lostark.game.onstove.com/armories/characters/{}/profiles"
+    return base_url.format(character_name)
 
-@bot.event
-async def on_rady():
-    print(bot.user)
-    
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'pong! {round(round(bot.latency, 4)*1000)}ms')
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
 
-bot.run(discord_token)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!캐릭터 '):
+        character_name = message.content[len('!캐릭터 '):].strip()
+        url = create_url(character_name)
+        
+        response = requests.get(url, headers=headers)
+        
+        if response.status_code == 200:
+            jsonObject = response.json()
+            
+            embed = discord.Embed(title="Lost Ark 캐릭터 정보", color=discord.Color.blue())
+            embed.add_field(name="서버 이름", value=jsonObject.get("ServerName"), inline=True)
+            embed.add_field(name="캐릭터 이름", value=jsonObject.get("CharacterName"), inline=True)
+            embed.add_field(name="클래스 이름", value=jsonObject.get("CharacterClassName"), inline=True)
+            embed.add_field(name="아이템 최대 레벨", value=jsonObject.get("ItemMaxLevel"), inline=True)
+            embed.set_thumbnail(url=jsonObject.get("CharacterImage"))
+            
+            await message.channel.send(embed=embed)
+        else:
+            await message.channel.send(f"오류: {response.status_code} {response.text}")
+
+client.run(discord_token)
--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 import requests
 import json
 from tokens import lostark_token
- 
+
 headers = {
-    'accept' : 'application/json',
-    'authorization' : lostark_token
+    'accept': 'application/json',
+    'authorization': lostark_token
 }
- 
 
 # 사용자가 입력한 캐릭터 이름을 기반으로 URL 생성
 def create_url(character_name):
-    base_url = "https://developer-lostark.game.onstove.com/characters/{}/siblings"
+    base_url = "https://developer-lostark.game.onstove.com/armories/characters/{}/profiles"
     return base_url.format(character_name)
 
 # 사용자 입력 받기
@@ -18,16 +17,20 @@ character_name = input("캐릭터 이름을 입력하세요: ")
 
 # URL 생성
 url = create_url(character_name)
- 
+
+# API 요청 보내기
 response = requests.get(url, headers=headers)
-jsonObject = response.json()
- 
-#결과값 표시 (결과값의 내용은 https://developer-lostark.game.onstove.com/usage-guide#http-basic 참조)
-print(response)
- 
-# json 출력 코드
-for list in jsonObject :
-    print(list.get("ServerName"))
-    print(list.get("CharacterName"))
-    print(list.get("CharacterClassName"))
-    print(list.get("ItemMaxLevel"))
+
+# 응답 상태 코드 확인
+if response.status_code == 200:
+    # JSON 응답 파싱
+    jsonObject = response.json()
+
+    # 결과값 표시
+    print("Server Name:", jsonObject.get("ServerName"))
+    print("Character Name:", jsonObject.get("CharacterName"))
+    print("Character Class Name:", jsonObject.get("CharacterClassName"))
+    print("Item Max Level:", jsonObject.get("ItemMaxLevel"))
+    print("Character Image URL:", jsonObject.get("CharacterImage"))
+else:
+    print("Error:", response.status_code, response.text)
